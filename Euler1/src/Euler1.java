1f80791399fd0332a030231d19d1801f32b40c41
Nivethan <n2nandak@uwaterloo.ca>
2014-04-30 10:36:02 -0400

    Euler Problem 1

---
+++ Euler1/src/Euler1.java
@@ -0,0 +1,21 @@
+
+public class Euler1 {
+
+	public static void main(String[] args) {
+
+		int sum = 0;
+		
+		for (int i=0;i<1000;i+=3)
+		{
+			sum=sum+i;
+		}
+		for (int j = 0;j<1000;j+=5)
+		{
+			if (j%3 != 0)
+				sum+=j;
+		}
+		System.out.println(sum);
+
+	}
+
+}
